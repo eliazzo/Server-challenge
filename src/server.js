e80c7b72b1a42619ec6dd85af8b34c7ffd00cdc5
eliazzo <bethazz@hotmail.co.uk>
2023-05-02 14:08:30 +0100

    challenge 3 and 4

--- src/server.js
+++ src/server.js
@@ -3,3 +3,56 @@ const express = require("express");
 const server = express();
 
 module.exports = server;
+
+// Challenge 1: Create a new route for the homepage
+// server.get("/", (request, response) => {
+//     response.send(`<h1>Hello world</h1>`)
+// })
+
+
+// Challenge 2: Create a new route at GET /colour // Challenge 3: Edit your GET /colour route to include a form in the HTML response.
+// server.get("/", (req, res) => {
+//     const hex = req.query.hex || "ff0000"; // defaults to white
+//     const html = `
+//       <style>
+//         body {
+//           background-color: #${hex};
+//         }
+//       </style>
+//       <form> 
+//         <label for="hex">Enter hex</label>
+//         <input name="hex" value="${hex}">
+//       </form>
+//     `;
+//     res.send(html);
+//   });
+
+// Challenge 4: Create a new route GET /cheese
+
+const cheeses = [];
+
+server.get("/", (request, response) => {
+  const html = `
+    <form method="POST">
+      <label for="cheese">Cheese Type</label>
+      <input name="name" type="text"/>
+
+      <label>Rating</label>
+      <input name="rating" type="range" min="0" max="5"/>
+
+      <button type="submit">Rate cheese</button>
+    </form>
+    <ul>
+    ${cheeses}
+    </ul>
+    `
+    response.send(html)
+})
+const bodyParser = express.urlencoded();
+
+server.post("/", bodyParser, (req, res) => {
+  const cheeseName = req.body.name;
+  const cheeseRating = req.body.rating;
+  cheeses.push(cheeseName, cheeseRating);
+  res.redirect("/")
+})
